b159ba2f0d0797004e935c327e1a1c82ad1cb3af
Younghwan Jang <yfaney@gmail.com>
2015-09-13 12:05:06 -0500

    Cleanup deprecated

---
+++ RTTMeasurer.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+import datetime
+import socket
+import sys
+
+NODE_LIST_FILE = ["192.168.0.16"]
+TEST_FILES = ["file_32B", "file_1K", "file_256K","file_512K", "file_1M"]
+
+MAX_LEN = 1048576
+BLOCK_SIZE = 8192
+DEBUG = True
+
+# Read node list from a file
+f = open("nodelist.txt")
+nodeList = f.readlines()
+while '' in nodeList:
+    nodeList.pop(nodeList.index(''))
+	
+for item in nodeList:
+    print item
+
+def receive(sock):
+        chunks = []
+        bytes_recd = 0
+        running = True
+        while running:
+            chunk = sock.recv(BLOCK_SIZE)
+            if ' ' in chunk:
+                chunks.append(chunk[:len(chunk)-1])
+                break
+            else:
+                chunks.append(chunk)
+        return ''.join(chunks)
+
+#create an INET, STREAMing socket
+sk = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM)
+#now connect to the web server on port 80
+# - the normal http port
+
+for node_list in NODE_LIST_FILE:
+    sk.connect(("192.168.0.16", 55700))
+    for fileName in TEST_FILES:
+        for i in range(1,4):
+            sk.send(fileName)
+            recvd = receive(sk)
+            #print recvd
+            #print (len(recvd))
+        sk.send(fileName)
+        startTime = datetime.datetime.now()
+        recvd = receive(sk)
+        endTime = datetime.datetime.now()
+        td = endTime - startTime
+        delay = td.seconds + float(td.microseconds/1000000.0)
+        #print recvd
+        if(DEBUG):
+            print (len(recvd))
+            print delay
--- spreadFilesToAllNodes.sh
+++ spreadFilesToAllNodes.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-#Making files: 32B, 1KB, 512KB, 1MB, 1GB
-#or 32B, 1KB, 256KB, 512KB, 1MB
-
-echo "Making files: 32B..."
-dd if=/dev/zero of=/tmp/file_32B  bs=1  count=1
-echo "Making files: 1KB..."
-dd if=/dev/zero of=/tmp/file_1KB  bs=1K  count=1
-echo "Making files: 512KB..."
-dd if=/dev/zero of=/tmp/file_512KB  bs=1K  count=512
-echo "Making files: 1MB..."
-dd if=/dev/zero of=/tmp/file_1MB  bs=1M  count=1
-echo "Making files: 1GB..."
-dd if=/dev/zero of=/tmp/file_1KB  bs=1G  count=1
-
-echo "Sending them to all nodes..."
-
-
 for dest in $(<nodelist.txt); do
   scp /tmp/file_32B ${dest}:remote/path/
 
