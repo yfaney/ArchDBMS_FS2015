1b5cc557f45ce88f512974f517e705da9b92666b
Younghwan Jang <yfaney@gmail.com>
2015-09-14 09:32:07 -0500

    Final update
    Update real nodes,
    Remove test nodes,
    Update writing CSV routine

--- RTTMeasurer.py
+++ RTTMeasurer.py
@@ -2,32 +2,41 @@
 import datetime
 import socket
 import sys
+import csv
 
-NODE_LIST_FILE = ["192.168.0.16"]
-TEST_FILES = ["file_32B", "file_1K", "file_256K","file_512K", "file_1M"]
+TEST_FILES = [{"name" : "file_32B", "size" : 32},
+                {"name" : "file_1K","size" : 1024},
+                {"name" : "file_256K","size" : 256 * 1024},
+                {"name" : "file_512K","size" : 512 * 1024},
+                {"name" : "file_1M","size" : 1024 * 1024}]
 
 MAX_LEN = 1048576
 BLOCK_SIZE = 2048
 DEBUG = True
+#At first, get the current node address
+
+MyAddress = socket.gethostname()
 
 # Read node list from a file
 f = open("nodelist.txt")
-nodeList = f.readlines()
+rawList = f.readlines()
 
-for item in nodeList:
-    print item.strip()
-    print socket.gethostbyname(item.strip())
-    
+nodeList = []
 
-print len(nodeList)
+for item in rawList:
+    if item != MyAddress:
+        print item.strip()
+        nodeList.append(socket.gethostbyname(item.strip()))
+
+print "Node Size: %d" % len(nodeList)
 
 def receive(sock):
         chunks = []
         bytes_recd = 0
-        running = True
-        while running:
+#        running = True
+        while True:
             chunk = sock.recv(BLOCK_SIZE)
-            if ' ' in chunk:
+            if chunk[len(chunk)-1:] == '\n':
                 chunks.append(chunk[:len(chunk)-1])
                 break
             else:
@@ -40,24 +49,35 @@ sk = socket.socket(
 #now connect to the web server on port 80
 # - the normal http port
 
+#Log Structure: [ Source, Target, TImestamp, FileSize, Delay ]
+
+resultLog = []
 
 for node_list in nodeList:
     #sk.connect(("192.168.0.16", 55700))
-	sk.connect(socket.gethostbyname(node_list.strip()),55700)
-    for fileName in TEST_FILES:
+    sk.connect(node_list,55700)
+    for file in TEST_FILES:
         for i in range(1,4):
-            sk.send(fileName)
+            sk.send(file["name"])
             recvd = receive(sk)
             #print recvd
             #print (len(recvd))
-        sk.send(fileName)
+        sk.send(file["name"])
         startTime = datetime.datetime.now()
         recvd = receive(sk)
         endTime = datetime.datetime.now()
-        td = endTime - startTime
-        delay = td.seconds + float(td.microseconds/1000000.0)
-        #print recvd
-        if(DEBUG):
-            print (len(recvd))
-            print delay
+        if 'File was not open' in recvd[len('File was not open')]:
+            print "File Error: File was not open"
+        else:
+            td = endTime - startTime
+            delay = td.seconds + float(td.microseconds/1000000.0)
+            #print recvd
+            if(DEBUG):
+                print (len(recvd))
+                print delay
+            resultLog.append([MyAddress, node_list, startTime, file["size"], delay])
     sk.close()
+
+with open("resultLog.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerows(resultLog)
